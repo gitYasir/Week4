5a98abf10fa049fff6b657c324aedd1abb965d2e
Yasir <yasir.ibrahim@hotmail.co.uk>
2022-11-29 17:47:24 +0000

    bubble sort started

---
+++ BubbleSort/BubbleSortApp/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace BubbleSortApp {
+    public class Program {
+        static void Main( string[] args ) {
+            Console.WriteLine( "Hello, World!" );
+        }
+        public static int[] BubbleSort( int[] arr ) {
+
+
+
+            return new[] { 1, 2 };
+        }
+    }
+}
---
+++ BubbleSort/BubbleSortTests/BubbleSortShould.cs
@@ -0,0 +1,8 @@
+namespace BubbleSortTests {
+    public class BubbleSortShould {
+
+        public void GivenArrayOfInts_BubbleSort_ReturnsOrderedArrayOfInts( int[] actual, int[] expected ) {
+            Assert.Pass();
+        }
+    }
+}
