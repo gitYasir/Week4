f1c8a5989837d72d754cc4405070c8c4c06e3cc2
Yasir <yasir.ibrahim@hotmail.co.uk>
2022-12-01 22:29:58 +0000

    practiced recursion

---
+++ Recursion/Recursion/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Recursion {
+    internal class Program {
+        static void Main( string[] args ) {
+            Console.WriteLine( $"SumLoop of 5: {SumLoop( 5 )}" );
+            Console.WriteLine( $"SumRecursion of 5: {SumRecursion( 5 )}" );
+        }
+
+        public static int SumLoop( int n ) {
+
+            int sum = 0;
+            for ( int i = 0; i <= n; i++ ) {
+
+                sum += i;
+
+            }
+            return sum;
+        }
+
+        public static int SumRecursion( int n ) {
+            int sum = n;
+            if ( n > 0 ) {
+                sum += SumRecursion( n - 1 );
+            }
+            return sum;
+        }
+
+    }
+}
